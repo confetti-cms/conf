c4aeddf1e4b8d5fc1bc91048745841b6754c850b
Reindert Vetter <reindert@plugandpay.nl>
2022-09-13 22:44:06 +0200

    Get file status untracked

--- app/services/status.go
+++ app/services/status.go
@@ -4,32 +4,76 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"strings"
+	"regexp"
 
 	"github.com/spf13/cast"
 )
 
+type Status string
+
+const (
+	StatusUntracked Status = "?"
+)
+
 type FileChange struct {
-	Path string
+	Path   string
+	Status Status
 }
 
 func ChangedFiles(dir string) []FileChange {
-	st := fmt.Sprintf("git -C %s status -s --porcelain", dir)
+	// Get all changes from git status in plain text
+	st := fmt.Sprintf("git -C %s status -s --porcelain --untracked-files=all", dir)
 	stdout, err := runCommand(st)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if stdout == "" {
-		return []FileChange{}
+	// Get all changes in FileChange objects
+	compiler := regexp.MustCompile(`(?P<path>[/A-z.]+)`)
+	matches := parseByRegex(stdout, compiler)
+	fileChanges := []FileChange{}
+	for _, match := range matches {
+		fileChanges = append(fileChanges, FileChange{
+			Path:   match["path"],
+			Status: StatusUntracked,
+		})
 	}
-	path := strings.Split(stdout, "\n")[0]
-	path = strings.Split(path, " ")[1]
-	file := FileChange{Path: path}
-	return []FileChange{file}
+	return fileChanges
 }
 
 func runCommand(command string) (string, error) {
 	out, err := exec.Command("/bin/sh", "-c", command).Output()
 
 	return cast.ToString(out), err
+}
+
+func parseByRegex(content string, compiler *regexp.Regexp) []map[string]string {
+	matches := compiler.FindAllStringSubmatch(content, -1)
+	result := make([]map[string]string, 0)
+	names := subExpNames(compiler)
+	for _, match := range matches {
+		mapped := make(map[string]string)
+		for iNames, name := range names {
+			if len(match) <= iNames {
+				continue
+			}
+			mapped[name] = match[iNames+1]
+		}
+		result = append(result, mapped)
+	}
+
+	return result
+}
+
+func subExpNames(compiler *regexp.Regexp) []string {
+	names := []string{}
+	for i, name := range compiler.SubexpNames() {
+		if i == 0 {
+			continue
+		}
+		if name == "" {
+			continue
+		}
+		names = append(names, name)
+	}
+	return names
 }
--- tests/get_changed_files_test.go
+++ tests/get_changed_files_test.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"src/app/services"
 	"testing"
 
@@ -12,9 +14,9 @@ import (
 
 const mockDir = "mock_generated"
 
-func Test_no_changed_files(t *testing.T) {
+func Test_no_changes(t *testing.T) {
 	// Given
-	dir := generateGitDir("no_changed_files")
+	dir := initTestGit("no_changes")
 	// When
 	changes := services.ChangedFiles(dir)
 	// Then
@@ -22,9 +24,9 @@ func Test_no_changed_files(t *testing.T) {
 	i.True(len(changes) == 0)
 }
 
-func Test_one_changed_file(t *testing.T) {
+func Test_one_new_file(t *testing.T) {
 	// Given
-	dir := generateGitDir("one_changed_file")
+	dir := initTestGit("one_new_file")
 	touchFile(dir, "logo.svg")
 	// When
 	changes := services.ChangedFiles(dir)
@@ -34,7 +36,57 @@ func Test_one_changed_file(t *testing.T) {
 	i.Equal("logo.svg", changes[0].Path)
 }
 
-func generateGitDir(testDir string) string {
+func Test_multiple_new_files(t *testing.T) {
+	// Given
+	dir := initTestGit("multiple_new_files")
+	touchFile(dir, "logo.png")
+	touchFile(dir, "logo.svg")
+	// When
+	changes := services.ChangedFiles(dir)
+	// Then
+	i := is.New(t)
+	i.True(len(changes) == 2)
+	i.Equal("logo.png", changes[0].Path)
+	i.Equal("logo.svg", changes[1].Path)
+}
+
+func Test_new_path_with_file(t *testing.T) {
+	// Given
+	dir := initTestGit("new_path_with_file")
+	touchFile(dir, "images/logo.svg")
+	// When
+	changes := services.ChangedFiles(dir)
+	// Then
+	i := is.New(t)
+	i.True(len(changes) == 1)
+	i.Equal("images/logo.svg", changes[0].Path)
+}
+
+func Test_file_with_capital_letter(t *testing.T) {
+	// Given
+	dir := initTestGit("new_path_with_file")
+	touchFile(dir, "images/Logo.svg")
+	// When
+	changes := services.ChangedFiles(dir)
+	// Then
+	i := is.New(t)
+	i.True(len(changes) == 1)
+	i.Equal("images/Logo.svg", changes[0].Path)
+}
+
+func Test_status_untracked(t *testing.T) {
+	// Given
+	dir := initTestGit("new_path_with_file")
+	touchFile(dir, "Logo.svg")
+	// When
+	changes := services.ChangedFiles(dir)
+	// Then
+	i := is.New(t)
+	i.True(len(changes) == 1)
+	i.Equal(services.StatusUntracked, changes[0].Status)
+}
+
+func initTestGit(testDir string) string {
 	currentDir, err := os.Getwd()
 	if err != nil {
 		log.Println(err)
@@ -59,8 +111,13 @@ func generateGitDir(testDir string) string {
 	return dir
 }
 
-func touchFile(dir string, name string) {
-	file, err := os.Create(dir + "/" + name)
+func touchFile(dir string, fileName string) {
+	// Ensure dir exists
+	subDir := path.Dir(fileName)
+	fullDir := filepath.Join(dir, subDir)
+	os.MkdirAll(fullDir, 0755)
+	// Create file
+	file, err := os.Create(filepath.Join(dir, fileName))
 	if err != nil {
 		log.Fatal(err)
 	}
