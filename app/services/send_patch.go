b126b0a13ea4b1d7cab9edb5e11186ef7743f5f0
Reindert Vetter <reindertvetter@gmail.com>
2023-02-27 22:44:55 +0100

    Send files to domain

--- app/services/send_patch.go
+++ app/services/send_patch.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 type PatchBody struct {
@@ -14,33 +15,32 @@ type PatchBody struct {
 }
 
 func SendPatch(requestBody PatchBody) error {
-	url := "http://localhost:3001/api/source"
+	url := "http://api.localhost/parser/source"
 	method := "PATCH"
 	payloadB, err := json.Marshal(requestBody)
 	if err != nil {
 		return err
 	}
 	payload := bytes.NewReader(payloadB)
-
-	client := &http.Client{}
+	// Create request
+	client := &http.Client{Timeout: 10 * time.Second}
 	req, err := http.NewRequest(method, url, payload)
 	if err != nil {
 		return err
 	}
 	req.Header.Add("Accept-Language", "application/json")
 	req.Header.Add("Content-Type", "application/json")
-
+	// Do request
 	res, err := client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
-
+	// Create response
 	responseBody, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		println("Error response: " + string(responseBody))
 		return err
 	}
-
 	return nil
 }
