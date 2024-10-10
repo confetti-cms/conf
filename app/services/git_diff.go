6e23c91dfcd9a45afe275cca895dafd42a3e7dde
reindert-vetter <reindertvetter@gmail.com>
2024-10-10 23:40:48 +0200

    use --binary for patches for binary files

--- app/services/git_diff.go
+++ app/services/git_diff.go
@@ -18,18 +18,35 @@ func GetPatchSinceCommit(commit, path string, isNew bool) string {
 }
 
 func GetPatchSinceCommitE(commit, file string, isNew bool) (string, error) {
+	// Determine if the file is binary and set the binary flag
+	binaryFlag := ""
+	if isBinaryFile(file) {
+		binaryFlag = " --binary"
+	}
+
 	// Get tracked changes from git diff in patch format
-	st := fmt.Sprintf("cd %s && git diff %s -- %s", config.Path.Root, commit, file)
+	st := fmt.Sprintf("cd %s && git diff %s%s -- %s", config.Path.Root, commit, binaryFlag, file)
 	out, err := RunCommand(st)
 	if err != nil {
 		return "", err
 	}
-	if strings.Trim(out, "\n") != "" || isNew == false {
+	if strings.Trim(out, "\n") != "" || !isNew {
 		return out, err
 	}
 	// If no results; get untracked changes
-	st = fmt.Sprintf("cd %s && git diff -- /dev/null %s", config.Path.Root, file)
+	st = fmt.Sprintf("cd %s && git diff -- /dev/null %s -- %s", config.Path.Root, binaryFlag, file)
 	// Unknown way err is not nil
 	out, _ = RunCommand(st)
 	return out, nil
+}
+
+func isBinaryFile(file string) bool {
+	// This needs to be the same list as the list in the parser service
+	textExtensions := []string{".txt", ".md", ".json", ".xml", ".html", ".css", ".js", ".go", ".java", ".py", ".rb", ".php", ".c", ".cpp", ".h", ".hpp", ".cs", ".ts", ".sql", ".sh", ".bat", ".ps1", ".psm1", ".psd1", ".ps1xml", ".pssc", ".psc1", ".php", ".phtml", ".inc", ".tpl", ".twig"}
+	for _, ext := range textExtensions {
+		if strings.HasSuffix(file, ext) {
+			return false
+		}
+	}
+	return true
 }
