181ce43b2b96c227a1a9a0f57ba0bd4038623124
reindert-vetter <reindertvetter@gmail.com>
2025-07-22 23:31:44 +0200

    Add ContainerQuery command and implement container querying functionality

--- app/console/commands/container_query.go
+++ app/console/commands/container_query.go
@@ -12,6 +12,7 @@ type ContainerQuery struct {
 	Directory       string `short:"d" flag:"directory" description:"Root directory of the project, defaults to the current directory"`
 	Environment     string `short:"e" flag:"environment" description:"The environment name in the config.json5 file"`
 	Organisation    string `short:"o" flag:"organisation" description:"The organisation name (e.g. agency-name is the organisation when repository is: 'agency-name/website-name')"`
+	Repository      string `short:"r" flag:"repository" description:"The repository name (e.g. website-name is the repository when repository is: 'agency-name/website-name')"`
 	Verbose         bool   `short:"v" description:"Show events"`
 	VeryVerbose     bool   `short:"vv" description:"Show more events"`
 	VeryVeryVerbose bool   `short:"vvv" description:"Show all events"`
@@ -42,6 +43,7 @@ func (l ContainerQuery) Handle(c inter.Cli) inter.ExitCode {
 		c.Info("Use directory: %s", root)
 	}
 
+	// Get Environment for querying containers
 	env := l.Environment
 	if env == "" {
 		fmt.Println("\n\033[34mConfetti container:log\n\033[0m")
@@ -52,6 +54,33 @@ func (l ContainerQuery) Handle(c inter.Cli) inter.ExitCode {
 		}
 	}
 
+	// Get the running environment, only to validate the user is authorized
+	runningEnv, err := services.GetEnvironmentByInput(c, env)
+	if err != nil {
+		// If we can't get the environment by the query env (e.g. "all environments"),
+		// We ask the user to select an running environment
+		fmt.Printf("We couldn't find the environment by the query env.\nPlease select an environment so we can check if you are authorized.\n")
+		runningEnv, err := services.GetEnvironmentByInput(c, "")
+		if err != nil {
+			c.Error(fmt.Sprintf("Error getting environment: %s", err))
+			return inter.Failure
+		}
+	}
+
+	// Get all basic information about the containers the containers
+	query := services.QueryContainerOptions{
+		Environment:          env,
+		UmbrellaOrganization: l.Organisation,
+		UmbrellaRepository:   l.Repository,
+	}
+	containers, err := services.GetContainers(c, runningEnv, query)
+	// Get the organisation name for the query
+	if err != nil {
+		c.Error(fmt.Sprintf("Error getting containers: %s", err))
+		return inter.Failure
+	}
+	organisationNames := services.WhereOrganisation(c, containers, query.UmbrellaOrganization)
+
 	fmt.Printf("\n\033[34mconf container:query --env=\"%s\"\n\033[0m", env)
 
 	// The watch is preventing the code from ever getting here
@@ -60,19 +89,41 @@ func (l ContainerQuery) Handle(c inter.Cli) inter.ExitCode {
 
 const AllEnvironments = "all environments"
 
-func GetEnvironmentName(c inter.Cli, envName string) (string, error) {
+func GetEnvironmentName(c inter.Cli, envName string) (string, string, error) {
 	appConfig, err := services.GetAppConfig()
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 	names := []string{AllEnvironments}
 	for _, environment := range appConfig.Environments {
 		names = append(names, environment.Name)
 	}
 	envName = c.Choice("You can narrow down your search by selecting an environment:", names...)
+	runningEnv := envName
 	if envName == AllEnvironments {
 		envName = "*"
+		if len(names) < 2 {
+		runningEnv = names[1]
 	}
 
 	return envName, nil
+}
+
+func WhereOrganisation(c inter.Cli, containers []Container, organisation string) []ContainerInformationWithInformation {
+	orgNames := map[string]ContainerInformationWithInformation{}
+	for _, container := range containers {
+		if container.UmbrellaOrganization != "" {
+			orgNames[container.UmbrellaOrganization] = container
+		}
+	}
+	orgNames := []string{}
+	for orgName := range orgNames {
+		orgNames = append(orgNames, orgName)
+	}
+
+	
+
+
+	
+	return containers
 }
---
+++ app/services/container.go
@@ -0,0 +1,80 @@
+package services
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"src/config"
+
+	"github.com/confetti-framework/framework/inter"
+)
+
+type ContainerInformation struct {
+}
+
+type ContainerInformationWithInformation struct {
+	ID                   string                 `json:"id"`
+	Locator              string                 `json:"locator"`
+	SourceOrganization   string                 `json:"source_organization"`
+	SourceRepository     string                 `json:"source_repository"`
+	UmbrellaOrganization string                 `json:"umbrella_organization"`
+	UmbrellaRepository   string                 `json:"umbrella_repository"`
+	Name                 string                 `json:"name"`
+	Target               string                 `json:"target"`
+	Status               string                 `json:"status"`
+	Ports                []string               `json:"ports"`
+	NetworkName          string                 `json:"network_name"`
+	Environment          EnvironmentInformation `json:"environment"`
+}
+
+type EnvironmentInformation struct {
+	Name  string `json:"name"`
+	Stage string `json:"stage"`
+}
+
+type QueryContainerOptions struct {
+	Environment          string `json:"environment"`
+	UmbrellaOrganization string `json:"umbrella_organization"`
+	UmbrellaRepository   string `json:"umbrella_repository"`
+}
+
+func GetContainers(cli inter.Cli, runningEnv Environment, options QueryContainerOptions) ([]ContainerInformationWithInformation, error) {
+	baseUrl := GetOrchestratorContainerListUrl()
+	u, err := url.Parse(baseUrl)
+	if err != nil {
+		return nil, err
+	}
+	q := u.Query()
+	if options.Environment != "" {
+		q.Set("environment", options.Environment)
+	}
+	if options.UmbrellaOrganization != "" {
+		q.Set("umbrella_organization", options.UmbrellaOrganization)
+	}
+	if options.UmbrellaRepository != "" {
+		q.Set("umbrella_repository", options.UmbrellaRepository)
+	}
+	u.RawQuery = q.Encode()
+
+	resp, err := Send(cli, u.String(), nil, http.MethodPut, runningEnv, "")
+	if err != nil {
+		return nil, err
+	}
+
+	type responseData struct {
+		Data []ContainerInformationWithInformation `json:"data"`
+	}
+	var result responseData
+	err = json.Unmarshal([]byte(resp), &result)
+	if err != nil {
+		return nil, err
+	}
+	return result.Data, nil
+}
+
+func GetOrchestratorContainerListUrl() string {
+	if config.App.Local {
+		return OrchestratorApiLocalhost + "/container_list"
+	}
+	return OrchestratorApiDefault + "/container_list"
+}
